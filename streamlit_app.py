ad021ac80bea4e3191c026f1391401b90bdf9b75
Mathi-snowflake <104988131+Mathi-snowflake@users.noreply.github.com>
2022-05-05 18:49:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+Streamlit.Text("Oats with dates,Almond,cashew,apple")
+Streamlit.Text("Soak it for half an hour and Grind to a smooth liquid")
