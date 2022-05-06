555e6149e6867230b97a3640dc628a418e977dc4
Mathi-snowflake <104988131+Mathi-snowflake@users.noreply.github.com>
2022-05-06 17:38:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Banana','Peach'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-If fruits_to_show == NULL Then
+If fruits_to_show = NULL Then
 streamlit.dataframe(my_fruit_list)
 Else
 End If
