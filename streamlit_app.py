ac5126d8be138f1d8e89dc958274ae6c7ba29e67
Mathi-snowflake <104988131+Mathi-snowflake@users.noreply.github.com>
2022-05-06 18:08:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,5 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Banana','Peach'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-if fruits_selected == false: streamlit.dataframe(my_fruit_list)
+if fruits_selected: streamlit.dataframe(my_fruit_list)
+  else : streamlit.dataframe(my_fruit_list)
