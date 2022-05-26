f8452a2d28de393277a66368f064291345fedf4f
Mathi-snowflake <104988131+Mathi-snowflake@users.noreply.github.com>
2022-05-26 17:51:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,7 @@ streamlit.text(fruitvice_response.json())#just writes the data to the screen
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRECT_USER(), CURRECT_ACCOUNT() , CURRECT_REGION() ")
-#my_data_row = my_cur.fetchone()
-#streamlit.text(my_data_row)
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
