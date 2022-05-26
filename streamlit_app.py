fc661bc0dd98a6e6dea60fe0b68d2b8efb275e02
Mathi-snowflake <104988131+Mathi-snowflake@users.noreply.github.com>
2022-05-26 17:45:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,8 @@ streamlit.write('The user entered - ',fruit_choice)
 fruitvice_response = requests.get("https://www.fruityvice.com/api/fruit/" +fruit_choice)
 streamlit.text(fruitvice_response.json())#just writes the data to the screen
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select currect_user(), current_region(), currect_account()")
+my_data_row = my_cur.fetchone()
+streamlit.text(my_data_row)
